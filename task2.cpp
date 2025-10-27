109275c027656876f297eec1177d584cf837d366
hiraz18 <zaidi.hira20@gmail.com>
2025-10-27 15:47:12 +0530

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-int isPrime(int num) {
-    if (num < 2) return 0;
-    for (int i = 2; i*i <= num; i++) {
-        if (num % i == 0) return 0;
-    }
-    return 1;
-}
-
-int main() {
-    int start, end;
-    printf("Enter the range (start end): ");
-    scanf("%d %d", &start, &end);
-    printf("Prime numbers between %d and %d:\n", start, end);
-    for (int i = start; i <= end; i++) {
-        if (isPrime(i))
-            printf("%d ", i);
-    }
-    printf("\n");
-    return 0;
+main() {
+	int lowerl, upperl, i, j, primet;
+	
+	printf("Enter the lower limit: ");
+	scanf("%d", &lowerl);
+	printf("Enter the upper limit: ");
+	scanf("%d", &upperl);
+	printf("The prime numbers between %d-%d are: \n", lowerl, upperl);
+	
+	for(i=lowerl; i<=upperl; i++) {
+		if(i<2)
+			continue;
+		primet = 1;
+		for(j=2; j <= i/2; j++) {
+			if(i%j == 0) {
+				primet = 0;
+				break;
+			}
+		}
+		if(primet)
+			printf("%d ", i);
+	}
+	
+	printf("\n");
+	return 0;
 }
 
